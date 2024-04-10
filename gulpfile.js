eecb834719e680f90ff5ff9e4bb3043ce3fdd1d7
salted fish <35429238+hongbozha@users.noreply.github.com>
2024-04-10 20:51:10 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -82,7 +82,7 @@ gulp.task('watch', function() {
 	gulp.watch('src/js/**/*.js', ['js']);
 	gulp.watch('src/fonts/**/*.{tff,woff,woff2}', ['fonts']);
 	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-	gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
+	gulp.watch(['*.html', '_includes/*.html', '_layouts/*.html'], ['jekyll-rebuild']);
 });
 
 gulp.task('default', ['js', 'sass', 'fonts', 'browser-sync', 'watch']);
